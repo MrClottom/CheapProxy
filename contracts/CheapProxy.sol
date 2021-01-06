b73d33b5411a11cfcb8a1cd7275c8b216e870e79
MrClottom <philippe@dumo.net>
2021-01-06 13:59:22 +0100

    Fix indentation

--- contracts/CheapProxy.sol
+++ contracts/CheapProxy.sol
@@ -1,17 +1,17 @@
 object "CheapProxy" {
-  code {
-    codecopy(0, dataoffset("runtime"), datasize("runtime"))
-    return(0, datasize("runtime"))
-  }
-  object "runtime" {
     code {
-      calldatacopy(0, 0, calldatasize())
-      let addr := linkersymbol("proxyImplementation")
-      let success := delegatecall(gas(), addr, 0, calldatasize(), 0, 0)
-      if success {
-        return(0, returndatasize())
-      }
-      revert(0, returndatasize())
+        codecopy(0, dataoffset("runtime"), datasize("runtime"))
+        return(0, datasize("runtime"))
     }
-  }
+    object "runtime" {
+        code {
+            calldatacopy(0, 0, calldatasize())
+            let addr := linkersymbol("proxyImplementation")
+            let success := delegatecall(gas(), addr, 0, calldatasize(), 0, 0)
+            if success {
+                return(0, returndatasize())
+            }
+            revert(0, returndatasize())
+        }
+    }
 }
